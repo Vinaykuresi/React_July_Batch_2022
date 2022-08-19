de233caec1c0ec68145aba04922d0fa93c95bf6a
VinayKuresi <VinayKumar7565@gmail.com>
2022-08-19 08:57:22 +0530

    Nested Functions builtin functions

---
+++ JavaScript/builtIn.js
@@ -0,0 +1,30 @@
+function myFunction() {
+
+    var date = new Date()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+    var hour = date.getHours()
+
+    var setInstance = setInterval(()=>{
+         second++
+
+         if(second % 60 == 0){
+             minute++
+             second = 0
+         }
+         if(minute % 60 == 0){
+            hour++
+            minute = 0
+         }
+         
+         console.log("Minutes : " + minute + " Seconds : "+second)
+         document.getElementById("time").innerText = hour + " Hours " + minute + " minutes " + second + " seconds"
+     }, 1000)
+
+     setTimeout(() => {
+        clearInterval(setInstance)
+     }, 5000)
+
+    //  var value = prompt("Enter the Value : ")
+    //  document.getElementById("value").innerText = value
+ }
--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,17 +1,34 @@
-// function keyword to define the function
-// function signature(definition)
-function add(num1, num2){ // formal arguments or Parameters
-    // execution logic
-    sum = num1 + num2
+// // function keyword to define the function
+// // function signature(definition)
+// function add(num1, num2){ // formal arguments or Parameters
+//     // execution logic
+//     sum = num1 + num2
 
-    // returning the result
-    return sum
+//     // returning the result
+//     return sum
+// }
+
+// // function calling
+// // catching the returned value in result
+// result = add(10, 20) // actual arguments or Parameters
+
+// // output printing
+// console.log("The sum is : ", result)
+
+// nested function
+
+function greeting(greetings){
+
+    function userName(name){
+        let greet = greetings + name
+        console.log(greet)
+    }
+
+    userName("Vinay")
+
 }
 
-// function calling
-// catching the returned value in result
-result = add(10, 20) // actual arguments or Parameters
+// userName("Vinay")
 
-// output printing
-console.log("The sum is : ", result)
+greeting("Happy new year ")
 
